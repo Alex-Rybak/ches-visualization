87189b0deafc1bc83e99c75747baffd8e276d941
Alex-Rybak <30793837+Alex-Rybak@users.noreply.github.com>
2025-10-29 18:08:35 +0000

    Made it possible to display data from several datasets

--- chesvis.py
+++ chesvis.py
@@ -4,9 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 plt.style.use('_mpl-gallery')
-files = {"Canada": ["CA_data.csv", 2023], "Europe": ["EU_data.csv", 2024],
-         "Europe (historic)": ["EU_hist_data.csv", 1999], "Israel": ["IL_data.csv", 2021],
-         "LatAm": ["LA_data.csv", 2020]}
+files = {"Canada": "CA_data.csv", "Europe": "EU_data.csv",
+         "Europe (historic)":"EU_hist_data.csv", "Israel": "IL_data.csv",
+         "LatAm":"LA_data.csv"}
 
 
 def family_color(family):
@@ -31,52 +31,55 @@ def family_color(family):
             return 'mediumaquamarine'
         case 'Agrarian':
             return 'olivedrab'
-        case 'Israel':
-            return 'blue'
-        case 'Canada':
-            return 'maroon'
-        case 'LatAm':
-            return 'peru'
-        case _:
+        case 'Miscellaneous':
             return "dimgrey"
+        case 'CA':
+            return 'maroon'
+        case 'IL':
+            return 'blue'
+        case _:
+            return 'peru'
 
 
-def visualizeFile(set, this_year):
-    data = files[set]
-    fileName = data[0]
-
-    df = pd.read_csv(fileName)
+def visualizeFile(countries, years):
+    df = pd.DataFrame()
+    for set in countries:
+        fileName = files[set]
+        this_df = pd.read_csv(fileName)
+        if "family" not in this_df.columns:
+            this_df["family"] = this_df["country"]
+        df = pd.concat([df, this_df], ignore_index=True)
+    info = pd.DataFrame()
     # get data
-    try:
-        info = df.loc[df["year"] == this_year, ["country", "party", "family", "lrecon", "galtan"]].dropna()
-    except KeyError:
-        info = df.loc[df["year"] == this_year, ["country", "party", "lrecon", "galtan"]].dropna()
-    info["lrecon"] = pd.to_numeric(info["lrecon"], errors="coerce")
-    info["galtan"] = pd.to_numeric(info["galtan"], errors="coerce")
+    for a_year in years:
+        this_year = int(a_year)
+        this_info = df.loc[df["year"] == this_year, ["country", "party", "year", "family", "lrecon", "galtan"]].dropna()
+
+        this_info["lrecon"] = pd.to_numeric(this_info["lrecon"], errors="coerce")
+        this_info["galtan"] = pd.to_numeric(this_info["galtan"], errors="coerce")
+        this_info["year"] = pd.to_numeric(this_info["year"], errors="coerce")
+
+        info = pd.concat([info, this_info], ignore_index=True)
 
 
-    fig, ax = plt.subplots(figsize=(12, 7), num=f"Data for {set} in {this_year}")
+    fig, ax = plt.subplots(figsize=(12, 7), num=f"Data for {countries} in {years}")
     ax.axvline(x=5, color='black', linewidth=1.5, linestyle='-')
     ax.axhline(y=5, color='black', linewidth=1.5, linestyle='-')
-    ax.set_title(f"Data for {set} in {this_year}")
-    try:
-        sc = ax.scatter(info["lrecon"], info["galtan"],
-                        c=info["family"].apply(family_color),alpha=0.1, picker=True, s=60)
-    except KeyError:
-        sc = ax.scatter(info["lrecon"], info["galtan"],
-                        c=family_color(set), alpha=0.1, picker=True, s=60)
+    ax.set_title(f"Data for {countries} in {years}")
+    sc = ax.scatter(info["lrecon"], info["galtan"],c=info["family"].apply(family_color),alpha=0.1, picker=True, s=60)
 
-    try:
-        for _, row in info.iterrows():
-            ax.text(row["lrecon"]-0.02, row["galtan"]+0.01, row["party"],
+
+    for _, row in info.iterrows():
+        if len(years)==1:
+            label = row["party"]
+        else:
+            label = row["party"] + '\n' + str(row["year"])
+
+        ax.text(row["lrecon"]-0.02, row["galtan"]+0.01, label,
                     fontsize=5, color=family_color(row["family"]), alpha=0.8)
-    except KeyError:
-        for _, row in info.iterrows():
-            ax.text(row["lrecon"]-0.02, row["galtan"]+0.01, row["party"],
-                    fontsize=5, color=family_color(set), alpha=0.8)
 
     ax.set_xlabel("Economic Left–Right")
-    ax.set_ylabel("Social Progressive–Conservativ")
+    ax.set_ylabel("Social Progressive–Conservative")
     annot = ax.annotate(
         "", xy=(0, 0), xytext=(10, 10), textcoords="offset points",
         bbox=dict(boxstyle="round,pad=0.1", fc="white", alpha=0.9),
@@ -89,7 +92,7 @@ def visualizeFile(set, this_year):
         ind = event.ind[0]
         row = info.iloc[ind]
         annot.xy = (row["lrecon"], row["galtan"])
-        text = (f"{row['party']} ({row['country']})\n"
+        text = (f"{row['party']} ({row['country']})\n {row['year']}"
                 f"Econ: {row['lrecon']:.2f}, Soc: {row['galtan']:.2f}")
         annot.set_text(text)
         annot.set_visible(True)
@@ -99,12 +102,3 @@ def visualizeFile(set, this_year):
 
     plt.tight_layout(pad=3.0)
     plt.show()
-
-
-
-
-
-
-
-
-
--- runvis.py
+++ runvis.py
@@ -1,3 +1,9 @@
 from chesvis import visualizeFile
 
-visualizeFile("Israel", 2022)
+print("Enter the countries and years you wish to see. The options are Canada, Europe, Europe (historic), Israel, LatAm")
+sets = input("Enter the countries with \", \" between them: ").split(", ")
+print(sets)
+years = input("Enter the years in a similar format: ").split(", ")
+print(years)
+
+visualizeFile(sets,years)
